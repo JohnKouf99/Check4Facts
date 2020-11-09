ed1c568761ac2c2848ed2e717929989f7d5a1244
Asteris <asteris.gian@gmail.com>
2020-11-09 17:33:50 +0200

    Search and Harvest endpoint implemented. Works as intended. Store to database is missing.

--- check4facts/api/api.py
+++ check4facts/api/api.py
@@ -1,8 +1,15 @@
+import os
+import yaml
+
+from flask import request
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 
 from check4facts.api.init import create_app
 from check4facts.models.models import Statement
+from check4facts.config import DirConf
+from check4facts.scripts.search import SearchEngine
+from check4facts.scripts.harvest import Harvester
 
 app = create_app()
 ma = Marshmallow(app)
@@ -39,48 +46,44 @@ resources_schema = ResourceSchema(many=True)
 
 @app.route('/search_harvest', methods=['POST'])
 def search_harvest():
-    # TODO add search.run() and harvest.run() here and send back results.
-    # statement = statement_schema.dump(request.json)
-    return resources_schema.jsonify([
-        {
-            "body": "string",
-            "fileFormat": "PDF",
-            "harvestDate": "2020-11-06T10:58:30.672Z",
-            "harvestIteration": 0,
-            "id": 0,
-            "simParagraph": "string",
-            "simSentence": "string",
-            "statement": {
-                "author": "string",
-                "id": 0,
-                "mainArticleText": "string",
-                "mainArticleUrl": "string",
-                "registrationDate": "2020-11-06T10:58:30.672Z",
-                "statementDate": "2020-11-06T10:58:30.672Z",
-                "statementSources": [
-                    {
-                        "id": 0,
-                        "snippet": "string",
-                        "title": "string",
-                        "url": "string"
-                    }
-                ],
-                "subTopics": [
-                    {
-                        "id": 0,
-                        "name": "string"
-                    }
-                ],
-                "text": "string",
-                "topic": {
-                    "id": 0,
-                    "name": "string"
-                }
-            },
-            "title": "string",
-            "url": "string"
-        }
-    ])
+    path = os.path.join('../../', DirConf.CONFIG_DIR, 'search_config.yml')
+    with open(path, 'r') as f:
+        search_params = yaml.safe_load(f)
+    se = SearchEngine(**search_params)
+    statement = request.json
+    claims = [statement.get('text')]
+    # Using first element only for the result cause only one statement is being checked.
+    search_results = se.run(claims)[0]
+
+    # Clean results from pdf/doc links.
+    if 'fileFormat' not in search_results:
+        search_results['fileFormat'] = 'html'
+    search_results['fileFormat'] = search_results['fileFormat'].fillna('html')
+    search_results = search_results[search_results['fileFormat'] == 'html']
+
+    path = os.path.join('../../', DirConf.CONFIG_DIR, 'harvest_config.yml')
+    with open(path, 'r') as f:
+        harvest_params = yaml.safe_load(f)
+    h = Harvester(**harvest_params)
+    articles = [{
+        'c_id': statement.get('id'),
+        'c_text': statement.get('text'),
+        'articles': search_results}]
+    # Using first element only for the result cause only one statement is being checked.
+    harvest_results = h.run(articles)[0]
+    response_result = []
+    for row in harvest_results.itertuples():
+        response_result.append({
+            'url': row.url,
+            'title': row.title,
+            'body': row.body,
+            'simParagraph': row.sim_par,
+            'simSentence': row.sim_sent,
+        })
+
+    # TODO save harvest results(Resources) to database.
+    return resources_schema.jsonify(response_result)
+
 
 # Successfully returns the list of all statements in database.
 @app.route('/statement')
