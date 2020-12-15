f41337c5c8b7a07132bf594726bf2f6010975899
Asteris <asteris.gian@gmail.com>
2020-12-15 12:38:10 +0200

    Using Celery message(task) queue instead of threads for better performance of app.

--- check4facts/api/api.py
+++ check4facts/api/api.py
@@ -1,39 +1,24 @@
-import os
-
-import yaml
-from flask import request, jsonify
+from celery import Celery
 from flask_cors import CORS
-
-
+from flask import request, jsonify
 from check4facts.api.init import create_app
-from check4facts.api.tasks import analyze_task, train_task
-from check4facts.config import DirConf
-from check4facts.database import DBHandler
+from check4facts.api.tasks import analyze_task
 
 app = create_app()
-CORS(app)
+app.config['CELERY_BROKER_URL'] = 'sqla+postgresql://check4facts@localhost:5432/check4facts'
 
-path = os.path.join(DirConf.CONFIG_DIR, 'db_config.yml')  # while using uwsgi
-with open(path, 'r') as f:
-    db_params = yaml.safe_load(f)
-dbh = DBHandler(**db_params)
+client = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+client.conf.update(app.config)
+CORS(app)
 
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
     statement = request.json
 
-    analyze_task(statement, dbh)
+    task = analyze_task.apply_async(kwargs={'statement': statement})
 
-    return jsonify({'started': True})
-
-
-@app.route('/train', methods=['GET'])
-def train():
-
-    train_task(dbh)
-
-    return jsonify({'get': True})
+    return jsonify({'started': True, 'taskId': task.task_id})
 
 
 if __name__ == '__main__':
